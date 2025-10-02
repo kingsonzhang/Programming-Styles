1c3d5b4ea2d556b8a79e35d549b39fae5f68faa7
Kingson Zhang <kingson.x.zhang@gmail.com>
2025-10-02 12:35:22 -0700

    Edited Assignment One to allow file input from CLI

--- Week1/Main.js
+++ Week1/Main.js
@@ -1,7 +1,7 @@
 const fileReader = require("fs");
 let dictionary = {};
-let stopWords = fileReader.readFileSync("stop_words.txt").toString().split(",");
-let book = fileReader.readFileSync("pride-and-prejudice.txt").toString().match(/[a-zA-Z]+/gm).map(x => x.toLowerCase());
+let stopWords = fileReader.readFileSync("../stop_words.txt").toString().split(",");
+let book = fileReader.readFileSync(process.argv[2]).toString().match(/[a-zA-Z]+/gm).map(x => x.toLowerCase());
 book.forEach(word => {
     if (!stopWords.includes(word) && word.length > 1){
         if (dictionary[word])
